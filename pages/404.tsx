7efbd301d5bd3f4938a55c3ca8634acdeb18dc8a
Vojtech Suchanek <suchavo3@cvut.cz>
2023-02-17 10:14:33 +0100

    404

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { PrismicRichText } from "@prismicio/react";
 import Link from "next/link";
 import styled from "styled-components";
-import { COLORS, components } from "../src/GlobalStyles";
+import { COLORS } from "../src/GlobalStyles";
 
 const StyledMainContainer = styled.main`
   width: 100vw;
@@ -14,18 +14,21 @@ const StyledMainContainer = styled.main`
   justify-content: center;
 `;
 
-const StyledImage =styled.img`
-box-shadow: 7px 7px ${COLORS.pink};
-`
+const StyledImage = styled.img`
+  box-shadow: 7px 7px ${COLORS.pink};
+`;
 
 export default function Error({ error }) {
   console.log(error);
   return (
     <StyledMainContainer>
-      <StyledImage src={error.data.error_image.url} alt={error.data.error_image.alt} />
-      <PrismicRichText field={error.data.error_message} components={components} />
+      <StyledImage
+        src={error.data.error_image.url}
+        alt={error.data.error_image.alt}
+      />
+      <PrismicRichText field={error.data.error_message} />
       <Link href={"/"}>
-        <PrismicRichText field={error.data.home} components={components}/>
+        <PrismicRichText field={error.data.home} />
       </Link>
     </StyledMainContainer>
   );
